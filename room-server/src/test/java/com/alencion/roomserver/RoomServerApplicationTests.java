c2569a11af15909fc44b6a75eec64f8ab87a9a86
JJL <rhqnrdl625@naver.com>
2020-11-17 18:09:33 +0900

    init

---
+++ room-server/src/test/java/com/alencion/roomserver/RoomServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alencion.roomserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
