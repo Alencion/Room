6fc8451597346968de18dde3999d6f8a05cb3bb8
JJL <rhqnrdl625@naver.com>
2020-11-24 00:11:58 +0900

    add access log filter

--- room-server/src/main/java/com/alencion/roomserver/RoomServerApplication.java
+++ room-server/src/main/java/com/alencion/roomserver/RoomServerApplication.java
@@ -1,7 +1,12 @@
 package com.alencion.roomserver;
 
+import com.alencion.roomserver.filter.AccessLogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class RoomServerApplication {
@@ -10,4 +15,14 @@ public class RoomServerApplication {
 		SpringApplication.run(RoomServerApplication.class, args);
 	}
 
+	/**
+	 * filter 설정
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean<Filter> getFilterRegistrationBean(){
+		FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>(new AccessLogFilter());
+		registrationBean.addUrlPatterns("/*");
+		return registrationBean;
+	}
 }
