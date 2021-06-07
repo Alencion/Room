8f8de87636a2545e297aa9f3e4975c29dd4c59be
JeongJun Lee <rhqnrdl625@gmail.com>
2021-06-07 15:25:45 +0900

    chore: set cors disabled for develop

--- room-server/src/main/java/com/alencion/roomserver/config/WebSecurityConfig.java
+++ room-server/src/main/java/com/alencion/roomserver/config/WebSecurityConfig.java
@@ -27,7 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().anonymous()
+        http.csrf().disable()
+                .cors().disable().anonymous()
                 .and()
                 .authorizeRequests()
                 .antMatchers("/", "/login", "/static/css/**", "/static/images/**",
