ab1960f6ea703fc5fc591c819d6631b61f55853c
Jeong Jun Lee <rhqnrdl625@naver.com>
2021-06-09 14:18:15 +0900

    [Room-Frontend] implement partially login-page  (#6)
    * reset front
    
    * feat: login page

--- room-front/src/index.js
+++ room-front/src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import LoginPage from './page/LoginPage'
+import MainPage from './page/MainPage'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/" component={MainPage} />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
