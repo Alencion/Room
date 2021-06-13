0a936377e7296374a8ebc780a96344a65e2137bc
LJJ <rhqnrdl625@gmail.com>
2021-06-13 13:55:30 +0900

    feat: create room page skeleton

--- room-front/src/constant/style.js
+++ room-front/src/constant/style.js
@@ -1,7 +1,9 @@
 export const COLOR = {
   BORDER_COLOR: '#999999',
+  SEPERATOR_COLOR: '#dddddd',
   BLACK: '#000000',
-  GREY: '#808088',
+  GREY: '#a0a0a0',
+  GREEN: '#3bb143',
   LIGHT_BLACK: '#333333',
   LIGHT_BLUE: '#40a9ff',
   LIGHT_GREY: '#f3f3f3',
