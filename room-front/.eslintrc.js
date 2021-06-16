9d21bb0c5c817c8e25b0398d4c2922d9bdb08d53
JeongJun Lee <rhqnrdl625@gmail.com>
2021-06-16 14:54:51 +0900

    chore: fix format in .eslintrc.js

--- room-front/.eslintrc.js
+++ room-front/.eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended", "react-app"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'react-app'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    semi: ["error", "never"],
-    quotes: ["error", "single"],
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
   },
-};
+}
