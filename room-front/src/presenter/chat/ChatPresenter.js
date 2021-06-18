3b408454d81036ed1b2dd45a82f0a75ef649e00b
LJJ <rhqnrdl625@gmail.com>
2021-06-18 10:41:25 +0900

    feaT: add time ago at chat

--- room-front/src/presenter/chat/ChatPresenter.js
+++ room-front/src/presenter/chat/ChatPresenter.js
@@ -3,6 +3,32 @@ import styled from 'styled-components'
 import { COLOR } from '../../constant/style'
 
 const ChatPresenter = ({ message }) => {
+  const timeAgo = () => {
+    console.log(message)
+    const today = new Date()
+    const timeValue = new Date(message.createdAt)
+
+    const betweenTime = Math.floor(
+      (today.getTime() - timeValue.getTime()) / 1000 / 60,
+    )
+    if (betweenTime < 1) return '방금전'
+    if (betweenTime < 60) {
+      return `${betweenTime}분전`
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60)
+    if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간전`
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24)
+    if (betweenTimeDay < 365) {
+      return `${betweenTimeDay}일전`
+    }
+
+    return `${Math.floor(betweenTimeDay / 365)}년전`
+  }
+
   return (
     <>
       <ChatWrapper>
@@ -10,7 +36,10 @@ const ChatPresenter = ({ message }) => {
           <img src={message.sender.picture + '&s=70'} alt={'user profile'} />
         </AvartarWrapper>
         <MessageWrapper>
-          <UserName>{message.sender.nickname}</UserName>
+          <UserName>
+            <b>{message.sender.nickname}</b>
+            <span>{timeAgo()}</span>
+          </UserName>
           <Message>{message.content}</Message>
         </MessageWrapper>
       </ChatWrapper>
@@ -52,8 +81,13 @@ const MessageWrapper = styled.div`
 `
 
 const UserName = styled.p`
-  font-weight: bold;
   margin-bottom: 5px;
+
+  & > span {
+    margin-left: 10px;
+    font-size: 0.7rem;
+    color: ${COLOR.LIGHT_BLACK};
+  }
 `
 
 const Message = styled.div``
