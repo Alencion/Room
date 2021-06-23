c0f67427530136cf865db88b9ee2cf10114fd71b
LJJ <rhqnrdl625@gmail.com>
2021-06-23 12:08:37 +0900

    chore: add ci workflow and Integrate test remove

--- room-api-server/src/test/java/com/alencion/api/RoomApiServerApplicationTests.java
+++ room-api-server/src/test/java/com/alencion/api/RoomApiServerApplicationTests.java
@@ -1,13 +1,7 @@
 package com.alencion.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RoomApiServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
